b6e6427cd41d2bbfcf9154286f0cbaf22edf91e2
ambujnmt <ambuj@nmttechnologies.com>
2024-11-21 13:15:03 +0530

    splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage('assets/images/splash.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
